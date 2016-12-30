89cee0f456c103dc3e0fcef47f142355f04a7234
Anthony Brown <chaoticferocity@gmail.com>
2016-12-30 16:49:37 -0600

    Load inactive lists

--- client/js/app.js
+++ client/js/app.js
@@ -13,6 +13,36 @@
       .otherwise('/')
     ;
   });
-  app.controller('homeCtrl', function(){});
+  app.controller('homeCtrl', function($timeout){
+    var ctrl = this;
+
+    ctrl.showInactive = false;
+
+    ctrl.active = [
+      {store:"Walmart", title:"list name", created:new Date()},
+      {store:"Target", title:"list name", created:new Date()}
+    ];
+
+    ctrl.inactive = [];
+
+    ctrl.loadInactive = function(){
+      if (ctrl.showInactive === false){
+        ctrl.loadingInactive = true;
+
+        $timeout(function(){
+          ctrl.inactive = [
+            {store:"Walmart", title:"list name", created:new Date()},
+            {store:"Target", title:"list name", created:new Date()}
+          ];
+          ctrl.showInactive = true;
+          ctrl.loadingInactive = false;
+        }, 200);
+
+      } else {
+        ctrl.showInactive = false;
+      }
+
+    };
+  });
   app.controller('listCtrl', function(){});
 }())
