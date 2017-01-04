08552076da1c5e8dd9acc6755bb550e021963875
Anthony Brown <chaoticferocity@gmail.com>
2017-01-04 16:40:46 -0600

    functionality to edit feature

--- client/js/app.js
+++ client/js/app.js
@@ -1,6 +1,6 @@
 (function(){
   var app = angular.module('List',['ngRoute']);
-  app.config(function($routeProvider){
+  app.config(function($routeProvider, $locationProvider){
     $routeProvider
       .when('/', {
         templateUrl: '/templates/home.html',
@@ -10,17 +10,22 @@
         templateUrl: '/templates/list.html',
         controller: 'listCtrl as ctrl'
       })
+      .when('/list', {
+        templateUrl: '/templates/list.html',
+        controller: 'listCtrl as ctrl'
+      })
       .otherwise('/')
     ;
+    $locationProvider.html5Mode({enabled:true, requireBase:false})
   });
-  app.controller('homeCtrl', function($timeout){
+  app.controller('homeCtrl', function($timeout, $location){
     var ctrl = this;
 
     ctrl.showInactive = false;
 
     ctrl.active = [
-      {store:"Walmart", title:"list name", created:new Date()},
-      {store:"Target", title:"list name", created:new Date()}
+      {id: 1, store:"Walmart", title:"list name", created:new Date()},
+      {id: 2, store:"Target", title:"list name", created:new Date()}
     ];
 
     ctrl.inactive = [];
@@ -31,8 +36,8 @@
 
         $timeout(function(){
           ctrl.inactive = [
-            {store:"Walmart", title:"list name", created:new Date()},
-            {store:"Target", title:"list name", created:new Date()}
+            {store:"Walmart", title:"list name", completed:new Date()},
+            {store:"Target", title:"list name", completed:new Date()}
           ];
           ctrl.showInactive = true;
           ctrl.loadingInactive = false;
@@ -43,6 +48,30 @@
       }
 
     };
+    ctrl.viewList = function(list){
+      $location.path('/list/'+list.id);
+    };
   });
-  app.controller('listCtrl', function(){});
+  app.controller('listCtrl', function($routeParams){
+    var ctrl = this;
+    ctrl.list = {};
+    ctrl.addItem = function(){
+      if(!ctrl.list.hasOwnProperty('items')){
+        ctrl.list.items = [];
+      }
+      ctrl.list.items.push({});
+    };
+
+    ctrl.removeItem = function(item){
+      ctrl.list.items.splice(ctrl.list.items.indexOf(item),1);
+    };
+
+    if($routeParams.hasOwnProperty('id')){
+      //load list
+
+    } else{
+      //create list
+      ctrl.edit = true;
+    }
+  });
 }())
--- server/server.js
+++ server/server.js
@@ -2,5 +2,9 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('../client'));
+
+app.all('/*', function(req, res, next){
+  res.sendFile('/index.html', {root: __dirname+'/../client'});
+})
 app.listen(8080);
-console.log('server started'); 
+console.log('server started');
